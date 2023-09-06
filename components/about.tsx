52d7617517ad98fbb36ec1ea63fb5bb5151e0f78
Renato Freitas <freitas-renato@outlook.com>
2023-09-06 17:22:54 -0400

    Add link to USP details, fix margins

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import {BsArrowRight} from "react-icons/bs";
-import {motion} from "framer-motion";
+import { BsArrowRight } from "react-icons/bs";
+import { HiMiniArrowUpRight } from "react-icons/hi2";
+import { motion } from "framer-motion";
 
 export default function About() {
     function textEmphasis(text: string) {
@@ -10,18 +11,25 @@ export default function About() {
     }
 
     return (
-        <motion.section 
-            className="flex flex-col mb-96 w-full lg:w-[70%] md:leading-8"
+        <motion.section
+            className="flex flex-col mb-20 lg:mb-44 w-full lg:w-[70%] md:leading-8"
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8 } }}
-            viewport={{ once: true, amount: 0.3}}
+            viewport={{ once: true, amount: 0.2 }}
         >
             <h2 className="text-4xl font-bold py-10">About Me</h2>
             <p className="text-lg text-zinc-400 py-3 text-justify ">
                 I&apos;m a Software Engineer born in Brazil, and currently
                 living in {textEmphasis("New York City")}. I have a bachelors
                 degree in {textEmphasis("Electrical and Computer Engineeing")}{" "}
-                from the University of São Paulo.
+                from the{" "}
+                <a
+                    target="_blank"
+                    href="https://en.wikipedia.org/wiki/University_of_S%C3%A3o_Paulo"
+                    className="font-semibold underline"
+                >
+                    University of São Paulo. <HiMiniArrowUpRight className="inline-block"/>
+                </a>
             </p>
             <p className="text-lg text-zinc-400 py-3 text-justify ">
                 Most of my professional experience consisists of developing and
@@ -33,15 +41,22 @@ export default function About() {
                 having worked on IoT projects that involved developing{" "}
                 {textEmphasis("web apps for data visualization")} and control,
                 as well as backend infrastructure for data storage and
-                processing, using {textEmphasis("React, Node.js, MySQL, InfluxDB and AWS")}.
+                processing, using{" "}
+                {textEmphasis("React, Node.js, MySQL, InfluxDB and AWS")}.
             </p>
             <p className="text-lg text-zinc-400 py-3 text-justify ">
                 Strong problem-solving skills and ability to work well in a team
                 environment. Excited to continue growing and building cool
                 projects.
             </p>
-            
-            <Link href="/about" className="flex max-w-fit items-center gap-3 group text-teal-400 font-semibold underline py-2">Read More <BsArrowRight className="group-hover:translate-x-2 transition-all"/></Link>
+
+            <Link
+                href="/about"
+                className="flex max-w-fit items-center gap-3 group text-teal-400 font-semibold underline py-2"
+            >
+                Read More{" "}
+                <BsArrowRight className="group-hover:translate-x-2 transition-all" />
+            </Link>
         </motion.section>
     );
 }
