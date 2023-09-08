9f6afa53fccb923bdf611614ded517988acf3ab8
Renato Freitas <freitas-renato@outlook.com>
2023-09-08 15:38:13 -0400

    Animate contact section when in view

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,28 +1,39 @@
+"use client";
+
 import { socials } from "@/lib/data";
+import { motion } from "framer-motion";
 
 export default function Contact() {
     return (
-        <section>
-            <div className="flex flex-col items-center justify-center mb-36">
-                <h2 className="text-5xl font-semibold mb-10">Get In Touch</h2>
-                <p className="text-zinc-400 text-center mb-6 max-w-lg text-lg">
-                    I&apos;m currently looking for new opportunities to work in
-                    NYC or remote anywhere in the USA. Feel free to contact me
-                    if you have any questions or just want to say hi!
-                </p>
-                <div className="flex items-center gap-4">
-                    {socials.map((social) => (
-                        <a
-                            key={social.name}
-                            target="_blank"
-                            href={social.url}
-                            className="text-4xl text-zinc-400 hover:text-zinc-300 transition-all cursor-pointer hover:scale-110 active:scale-105"
-                        >
-                            {social.icon}
-                        </a>
-                    ))}
-                </div>
+        <motion.section
+            className="flex flex-col items-center justify-center mb-36"
+            id="contact"
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8 } }}
+            viewport={{ once: true, amount: 0.2 }}
+        >
+            {/* <div className="flex flex-col items-center justify-center mb-36"> */}
+            <h2 className="text-5xl text-center font-semibold mb-10">
+                Get In Touch
+            </h2>
+            <p className="text-zinc-400 text-center mb-6 max-w-lg text-lg">
+                I&apos;m currently looking for new opportunities to work in NYC
+                or remote anywhere in the USA. Feel free to contact me if you
+                have any questions or just want to say hi!
+            </p>
+            <div className="flex items-center gap-4">
+                {socials.map((social) => (
+                    <a
+                        key={social.name}
+                        target="_blank"
+                        href={social.url}
+                        className="text-4xl text-zinc-400 hover:text-zinc-300 transition-all cursor-pointer hover:scale-110 active:scale-105"
+                    >
+                        {social.icon}
+                    </a>
+                ))}
             </div>
-        </section>
+            {/* </div> */}
+        </motion.section>
     );
 }
