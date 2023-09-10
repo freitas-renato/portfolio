927afb2f38eb1b75250f7c42cd1f8f549c007584
Renato Freitas <freitas-renato@outlook.com>
2023-09-10 19:22:18 -0400

    Update pill bg opacity

--- components/pill.tsx
+++ components/pill.tsx
@@ -1,6 +1,6 @@
 export default function Pill({ name }: { name: string }) {
     return (
-        <div className="rounded-full bg-teal-400/30 px-3 py-1 text-sm text-teal-300">
+        <div className="rounded-full bg-teal-400/20 px-3 py-1 text-sm text-teal-300">
             {name}
         </div>
     );
