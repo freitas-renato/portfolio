083cbeac8532e21e15dc14e40779a3a70fba2c1d
Renato Freitas <freitas-renato@outlook.com>
2023-09-14 11:38:48 -0400

    💥 Move project descriptions to another repository

--- lib/content.ts
+++ lib/content.ts
@@ -1,31 +1,63 @@
-import fs from 'fs';
-import path from 'path';
-import { compileMDX } from 'next-mdx-remote/rsc';
-import Carousel from '@/components/carousel';
-import CustomImage from '@/components/custom-image';
-import rehypeExternalLinks from 'rehype-external-links';
+import { compileMDX } from "next-mdx-remote/rsc";
+import Carousel from "@/components/carousel";
+import CustomImage from "@/components/custom-image";
+import rehypeExternalLinks from "rehype-external-links";
 
-const contentPath = path.join(process.cwd(), 'projects');
-
-export function getAvailableProjects(): string[] {
-    try {
-        const files = fs.readdirSync(contentPath).filter(file => path.extname(file) === '.mdx');
-
-        return files.map(file => file.replace(/\.mdx$/, ''));
-    } catch (error) {
-        console.error('Error reading directory:', error);
-        return [];
+export async function getAvailableProjects(): Promise<string[]> {
+    if (!process.env.CONTENT_API_URL) {
+        throw new Error("CONTENT_API_URL is not set");
     }
+
+    const api_url = new URL(process.env.CONTENT_API_URL + "/projects" || "");
+
+    const res = await fetch(api_url, {
+        headers: {
+            Accept: "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            Authorization: "Bearer " + process.env.CONTENT_API_TOKEN,
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error(`Error fetching projects: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    return data
+        .filter((item: any) => item.type === "file")
+        .map((project: any) => project.name.replace(/\.mdx$/, ""));
 }
 
-export async function getProjectBySlug(slug: string): Promise<Project | undefined> {
-    const availableProjects = getAvailableProjects();
+export async function getProjectBySlug(
+    slug: string,
+): Promise<Project | undefined> {
+    const availableProjects = await getAvailableProjects();
 
-    if (!availableProjects.includes(slug))
-        return undefined;
-        
-    // Read the file
-    const rawMDX = fs.readFileSync(path.join(contentPath, `${slug}.mdx`), 'utf8');
+    if (!availableProjects.includes(slug)) return undefined;
+
+    if (!process.env.CONTENT_RAW_URL) {
+        throw new Error("CONTENT_API_URL is not set");
+    }
+
+    const api_url = new URL(
+        process.env.CONTENT_RAW_URL + "/projects/" + slug + ".mdx" || "",
+    );
+
+    const res = await fetch(api_url, {
+        headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+            Authorization: "Bearer " + process.env.CONTENT_API_TOKEN,
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error(
+            `Error fetching project ${slug} metadata: ${res.statusText}`,
+        );
+    }
+
+    const rawMDX = await res.text();
 
     const { frontmatter, content } = await compileMDX<ProjectMetadata>({
         source: rawMDX,
@@ -36,15 +68,18 @@ export async function getProjectBySlug(slug: string): Promise<Project | undefine
         options: {
             parseFrontmatter: true,
             mdxOptions: {
-                format: 'mdx',
+                format: "mdx",
                 rehypePlugins: [
-                    [rehypeExternalLinks, {
-                        rel: ['nofollow'],
-                        target: '_blank',
-                    }]
-                ]
-            }
-        }
+                    [
+                        rehypeExternalLinks,
+                        {
+                            rel: ["nofollow"],
+                            target: "_blank",
+                        },
+                    ],
+                ],
+            },
+        },
     });
     frontmatter.slug = slug;
 
@@ -56,29 +91,58 @@ export async function getProjectBySlug(slug: string): Promise<Project | undefine
     return project;
 }
 
+export async function getProjectMetadataBySlug(
+    slug: string,
+): Promise<ProjectMetadata | undefined> {
+    if (!process.env.CONTENT_RAW_URL) {
+        throw new Error("CONTENT_API_URL is not set");
+    }
+
+    const api_url = new URL(
+        process.env.CONTENT_RAW_URL + "/projects/" + slug + ".mdx" || "",
+    );
+
+    const res = await fetch(api_url, {
+        headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+            Authorization: "Bearer " + process.env.CONTENT_API_TOKEN,
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error(
+            `Error fetching project ${slug} metadata: ${res.statusText}`,
+        );
+    }
+
+    const rawMDX = await res.text();
+
+    const { frontmatter } = await compileMDX<ProjectMetadata>({
+        source: rawMDX,
+        options: {
+            parseFrontmatter: true,
+            mdxOptions: {
+                format: "mdx",
+            },
+        },
+    });
+
+    frontmatter.slug = slug;
+
+    return frontmatter;
+}
+
 export async function getAllProjectsMetadata(): Promise<ProjectMetadata[]> {
-    const availableProjects = getAvailableProjects();
+    const availableProjects = await getAvailableProjects();
 
     const projects: ProjectMetadata[] = [];
-    
+
     for (const slug of availableProjects) {
-        // Read the file
-        const rawMDX = fs.readFileSync(path.join(contentPath, `${slug}.mdx`), 'utf8');
-
-        const { frontmatter } = await compileMDX<ProjectMetadata>({
-            source: rawMDX,
-            options: {
-                parseFrontmatter: true,
-                mdxOptions: {
-                    format: 'mdx',
-                }
-            }
-        });
-        frontmatter.slug = slug;
-
-        projects.push(frontmatter);
+        const frontmatter = await getProjectMetadataBySlug(slug);
+        if (frontmatter) {
+            projects.push(frontmatter);
+        }
     }
 
     return projects;
 }
-
